3f4dbe1079a3e8df016118f5a982e0d9e3da48c9
root <root@ip-172-17-0-128.ec2.internal>
2016-10-18 22:54:16 -0400

    remove old security groups parse based on sg id

--- ec2_relation.py
+++ ec2_relation.py
@@ -62,42 +62,6 @@ class Ec2_Relation():
         return(inbound,outbound)
 
 
-    def parse_sg(self,sg_id):
-        sg = boto3.resource("ec2", region_name=self.region).SecurityGroup(sg_id)
-        inbound = []
-        outbound = []
-        for rule in sg.ip_permissions:
-            for sg_ip in rule['IpRanges']:
-                port_range ='0 / 65535'
-                protocol = 'All'
-                if not rule['IpProtocol'] == '-1':
-                    port_range = rule['FromPort']
-                    if not rule['FromPort'] == rule['ToPort']:
-                        port_range = str(rule['FromPort']) + ' / ' + str(rule['ToPort'])
-                    protocol = rule['IpProtocol']
-                cip = sg_ip['CidrIp']
-                if cip[-2:] == '/0':
-                   cip=cip[:-2]
-                if cip[-3:] == '/32':
-                   cip=cip[:-3]
-                inbound.append ((cip,protocol,port_range))
-        for rule in sg.ip_permissions_egress:
-            for sg_ip in rule['IpRanges']:
-                port_range ='0 / 65535'
-                protocol = 'All'
-                if not rule['IpProtocol'] == '-1':
-                    port_range = rule['FromPort']
-                    if not rule['FromPort'] == rule['ToPort']:
-                        port_range = str(rule['FromPort']) + ' / ' + str(rule['ToPort'])
-                    protocol = rule['IpProtocol']
-                cip = sg_ip['CidrIp']
-                if cip[-2:] == '/0':
-                   cip=cip[:-2]
-                if cip[-3:] == '/32':
-                   cip=cip[:-3]
-                outbound.append ((cip,protocol,port_range))
-        return(inbound,outbound)
-
     def nodes_relations(self):
         ec2 = boto3.client("ec2", region_name=self.region)                      
         instances = ec2.describe_instances()['Reservations']
